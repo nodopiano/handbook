b43d87f1bd9a2b7f96dc16a54116d4d22bc01fae
Marco Bonomo <marco.bonomo@gmail.com>
2022-08-09 09:16:07 +0200

    fix style

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,26 +1,32 @@
 <template>
-    <div
-        class="grid grid-cols-1 md:grid-cols-4 bg-np-gray-50 dark:bg-np-black-500 text-np-black-500 dark:text-np-gray-50 font-sans">
+    <div class="bg-np-gray-50 dark:bg-np-black-500 ">
+        <div class="border-b dark:border-np-black-400 border-np-gray-100">
             <header
-                class="border-b dark:border-np-black-400 border-np-gray-100 col-span-4 py-2 flex justify-between items-center sticky">
+                class="max-w-7xl mx-auto  text-np-black-500 dark:text-np-gray-50  py-2 flex justify-between items-center sticky">
                 <nuxt-link to="/" class="justify-start flex">
                     <logo-dark class="-ml-8 h-12 dark:block hidden"></logo-dark>
                     <logo-light class="-ml-8 h-12 block dark:hidden"></logo-light>
                 </nuxt-link>
+                <div class="font-serif text-2xl font-bold">
+                    Handbook
+                </div>
                 <div class="px-8 justify-between flex flex-wrap">
                     <div class="block md:hidden px-4 cursor-pointer" @click="showSidebar">menu</div>
                     <ColorModePicker />
                 </div>
             </header>
+        </div>
+    <div
+        class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 bg-np-gray-50 dark:bg-np-black-500 text-np-black-500 dark:text-np-gray-50 font-sans">
         <div class="grid grid-cols-1 md:grid-cols-5 col-span-4 relative">
             <aside
-                class="col-span-1 w-full dark:border-np-black-400 bg-np-gray-100 dark:bg-np-black-500 border-np-gray-100 border-r p-8 absolute md:static -left-full"
+                class="col-span-1 w-full  dark:bg-np-black-500 p-8 absolute md:static -left-full"
                 :class="{ 'static -left-0': isVisible }">
-                <ContentNavigation v-slot="{ navigation }" class="text-np-black-300 dark:text-np-gray-300">
-                    <div v-for="link of navigation" :key="link._path" class="py-2">
+                <ContentNavigation v-slot="{ navigation }" class="text-np-black-300 dark:text-np-gray-300 w-full">
+                    <div v-for="link of navigation" :key="link._path" class="py-4">
                         <NuxtLink :to="link._path" class=" text-xl font-serif">{{ link.title }}</NuxtLink>
                         <div v-for="child of link.children" :key="child._path">
-                            <NuxtLink :to="child._path" class="py-8 text-lg font-serif ml-4">{{ child.title }}
+                            <NuxtLink :to="child._path" class="text-lg font-sans opacity-75 hover:opacity-100 mb-4 ml-4">{{ child.title }}
                             </NuxtLink>
                         </div>
                     </div>
@@ -36,6 +42,7 @@
             </main>
 
         </div>
+    </div>
     </div>
 </template>
 <script>
@@ -66,7 +73,7 @@ export default {
 }
 
 .content a {
-    @apply text-np-black-200 dark:text-np-black-100 underline;
+    @apply text-np-black-200 dark:text-np-black-300 underline;
 }
 
 h1,
@@ -75,7 +82,7 @@ h3,
 h4,
 h5,
 h6 {
-    @apply mb-4 text-np-black-200 dark:text-np-black-100;
+    @apply mb-4 text-np-black-200 dark:text-np-gray-600;
 }
 
 h1 {
