c9e198ba4afaa3af1db7195b885cd0cbe7636407
Marco Bonomo <marco.bonomo@gmail.com>
2023-03-21 08:41:21 +0100

    docus first commit

---
+++ app.config.ts
@@ -0,0 +1,26 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nodopiano - Handbook',
+    description: '',
+    image: 'logo-light.svg',
+    socials: {
+      instagram: 'nodogram',
+      github: 'nodopiano/'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: false
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'simple-icons:nuxtdotjs'
+        }
+      ]
+    }
+  }
+})
